730634622e61aca5158b05c338566206b48d053b
DwarfPorter <mrvlmor@gmail.com>
2022-06-13 20:36:29 +0300

    демонстрация решения 1 задачи 3 семинара

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int x = Prompt("Введите X => ");
+int y = Prompt("Введите Y =>");
+
+if (x == 0 || y == 0)
+{
+    System.Console.WriteLine("Одна из коорлдинат равна 0");
+}
+
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("1 четверть");
+}
+if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("2 четверть");
+}
+if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("3 четверть");
+}
+if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("4 четверть");
+}
