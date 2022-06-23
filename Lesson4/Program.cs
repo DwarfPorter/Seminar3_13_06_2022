89acfe71c0826fd1314c2cf72b6c37bed53138bb
DwarfPorter <mrvlmor@gmail.com>
2022-06-23 20:39:48 +0300

    lesson6

---
+++ Lesson4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+int Prompt(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+int SumA(int maxNumber)
+{
+    int answer = 0;
+    for (int i = 1; i <= maxNumber; i++)
+    {
+        answer += i;
+    }
+    return answer;
+}
+
+int SumAGauss(int maxNumber){
+    return maxNumber * (maxNumber + 1) / 2;
+}
+
+int numberA = Prompt("Введмте число > ");
+System.Console.WriteLine($"Сумма от 1 до {numberA} равна {SumA(numberA)}");
+System.Console.WriteLine($"Гаусс сумма {SumAGauss(numberA)}");
+
---
+++ check/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+double[] CreateArray(int length) 
+ { 
+ double[] array = new double[length]; 
+ for(int i = 0; i < array.Length; i++) 
+ { 
+     array[i] = new Random().Next(0, 100); 
+ } 
+ return array; 
+ }
+
+void PrintArray(double[] array){
+    foreach(var element in array){
+        System.Console.Write($"{element} ");
+    }
+}
+
+var array = CreateArray(5);
+PrintArray(array);
